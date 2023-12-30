87aae953699911d28a31aa010ce4a735e3bb4836
thiraphat.ch <thiraphat.ch@rmuti.ac.th>
2023-12-31 03:53:08 +0700

    Update UI and add logo (generated by blackbox.ai )

--- src/Title.js
+++ src/Title.js
@@ -5,6 +5,7 @@ function Title() {
     <div class="title-name">Products In Stock</div>
     <div className="desc">Welcome to our store! Here you can find a wide variety of products that are currently in stock.</div>
     <div className="logo" />
+    <img src="./img/bg1.png" alt=""/>
     </> );
 }
 
